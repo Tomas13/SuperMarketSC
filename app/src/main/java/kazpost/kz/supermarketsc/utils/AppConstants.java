9bec98e5144c38a3e6b27d3d6e379b472937c4bd
Tomas13 <tomas.mapoter@gmail.com>
2018-05-02 17:44:24 +0600

    changed url to production service

--- app/src/main/java/kazpost/kz/supermarketsc/utils/AppConstants.java
+++ app/src/main/java/kazpost/kz/supermarketsc/utils/AppConstants.java
@@ -8,11 +8,11 @@ public class AppConstants {
 
     public static final String PREF_NAME = "mobterminal_pref";
 
-    public static final String TIMESTAMP_FORMAT = "yyyyMMdd_HHmmss";
-
+//    public static final String TIMESTAMP_FORMAT = "yyyyMMdd_HHmmss";
 
 //    public static final String BASE_URL = "https://bot.post.kz/api/";
 //    public static final String BASE_URL = "http://pls-test.post.kz";
-    public static final String BASE_URL = "http://172.30.73.19:8088/";
+//    public static final String BASE_URL = "http://172.30.73.19:8088/";
+    public static final String BASE_URL = "http://172.30.73.113:8080/";
 
 }
